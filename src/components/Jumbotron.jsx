32cfb5d7c9b6be6c8ab7e900e047b82dd0e8b6ac
natashagils <natashagils@gmail.com>
2020-06-26 10:15:48 -0400

    First commit

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => (
+  <div
+    className="jumbotron jumbotron-fluid"
+    style={{ backgroundColor: "pink" }}
+  >
+    <h1> Employee Directory </h1>
+    <h6> Search for an employee by their name in the search bar below.</h6>
+  </div>
+);
